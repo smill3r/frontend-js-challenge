dfbbbcfa0cd6ae9911addf29ecec15cbd0f7c52a
Jose Smiller <josesmiller@paybook.me>
2024-09-21 15:36:36 +0200

    Change to angular animations for toast

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,21 +1,33 @@
+import { trigger, transition, style, animate } from '@angular/animations';
 import { Component } from '@angular/core';
 
 type ToastType = 'success' | 'error' | 'info';
 
 @Component({
   selector: 'app-toast',
-  template: `<div class="toast-container" *ngIf="show">
+  template: `<div class="toast-container" *ngIf="show" @toastAnimation>
     <div class="toast" [ngClass]="type">
       {{ message }}
       <button class="close-btn" (click)="hide()">x</button>
     </div>
   </div>`,
   styleUrls: ['./toast.component.scss'],
+  animations: [
+    trigger('toastAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateX(100%)' }),
+        animate('300ms ease-out', style({ transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [
+        animate('300ms ease-in', style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class ToastComponent {
   show = false;
   message = '';
-  type:  ToastType = 'success';
+  type: ToastType = 'success';
 
   constructor() {}
 
@@ -24,24 +36,16 @@ export class ToastComponent {
     this.type = type;
     this.show = true;
 
-    // Add the show class to animate
-    setTimeout(() => {
-      document.querySelector('.toast')?.classList.add('show');
-    }, 10); // Delay for the animation to apply after rendering
-
-    // Automatically hide the toast after 3 seconds
     setTimeout(() => {
       this.hide();
-    }, 3000);
+    }, 2000);
   }
 
   hide() {
-    // Remove the show class for hide animation
     document.querySelector('.toast')?.classList.remove('show');
 
-    // Wait for the animation to finish before actually hiding the toast
     setTimeout(() => {
       this.show = false;
-    }, 500); // Match the transition duration
+    }, 500);
   }
 }
